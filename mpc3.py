81616abfd6eb3925c65b7c206c868581f18e2120
Bangtam0402 <Bangtamngovu@gmail.com>
2026-04-23 08:05:22 +0700

    update booking system

---
+++ mpc3.py
@@ -0,0 +1,9 @@
+def display_menu():
+    print("\n====== MOVIE BOOKING SYSTEM ======")
+    print("1. Add Ticket")
+    print("2. Display Tickets")
+    print("3. Search Ticket")
+    print("4. Sort Tickets")
+    print("5. Statistics")
+    print("6. Save to File")
+    print("0. Exit")
