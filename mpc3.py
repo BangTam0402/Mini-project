d06c2a44b5e6aa8775288a1c40af99e5f767982d
Bangtam0402 <Bangtamngovu@gmail.com>
2026-04-23 08:10:42 +0700

    Add add_ticket function with ID check and price validation

--- mpc3.py
+++ mpc3.py
@@ -1,3 +1,4 @@
+tickets = []
 def display_menu():
     print("\n====== MOVIE BOOKING SYSTEM ======")
     print("1. Add Ticket")
@@ -7,3 +8,35 @@ def display_menu():
     print("5. Statistics")
     print("6. Save to File")
     print("0. Exit")
+def add_ticket(tickets):
+    ticket_id = input("ID: ")
+
+    # check duplicate
+    for t in tickets:
+        if t["id"] == ticket_id:
+            print("ID already exists!")
+            return tickets
+
+    movie = input("Movie: ")
+    customer = input("Customer: ")
+    seat = input("Seat: ")
+
+    try:
+        price = float(input("Price: "))
+        if price <= 0:
+            print("Price must > 0")
+            return tickets
+    except:
+        print("Invalid price!")
+        return tickets
+
+    tickets.append({
+        "id": ticket_id,
+        "movie": movie,
+        "customer": customer,
+        "seat": seat,
+        "price": price
+    })
+
+    print("Added successfully!")
+    return tickets
